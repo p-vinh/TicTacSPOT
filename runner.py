f1c31e5b91d90cc073d9da7b78577cf2e8149791
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-09-30 16:01:35 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -15,9 +15,9 @@ white = (255, 255, 255)
 # Create screen
 screen = pygame.display.set_mode(size)
 
-mediumFont = pygame.font.Font("OpenSans-Regular.ttf", 28)
-largeFont = pygame.font.Font("OpenSans-Regular.ttf", 40)
-moveFont = pygame.font.Font("OpenSans-Regular.ttf", 60)
+mediumFont = pygame.font.Font(".\\OpenSans-Regular.ttf", 28)
+largeFont = pygame.font.Font(".\\OpenSans-Regular.ttf", 40)
+moveFont = pygame.font.Font(".\\OpenSans-Regular.ttf", 60)
 
 
 user = None
@@ -43,16 +43,52 @@ while True:
         
         # Buttons
         playXButton = pygame.Rect((width / 8), (height / 2), (width / 4), 50)
-        playOButton = pygame.Rect(5 * (width / 8), (height / 2), (width / 4), 50)
-        
         playX = mediumFont.render("Play as X", True, black)
         playXRect = playX.get_rect()
         playXRect.center = playXButton.center
         pygame.draw.rect(screen, white, playXButton)
         screen.blit(playX, playXRect)
         
+        playOButton = pygame.Rect(5 * (width / 8), (height / 2), (width / 4), 50)
         playO = mediumFont.render("Play as O", True, black)
         playORect = playO.get_rect()
         playORect.center = playOButton.center
         pygame.draw.rect(screen, white, playOButton)
         screen.blit(playO, playORect)
+        
+        # If button clicked, start game
+        click, _, _ = pygame.mouse.get_pressed()
+        if click == 1:
+            mouse = pygame.mouse.get_pos()
+            if playXButton.collidepoint(mouse):
+                time.sleep(0.2)
+                user = ttt.X
+            elif playOButton.collidepoint(mouse):
+                time.sleep(0.2)
+                user = ttt.O
+
+    else:
+        # Draw game board
+        tile_size = 80
+        tile_origin = (width / 2 - (1.5 * tile_size),
+                       height / 2 - (1.5 * tile_size)) 
+        tiles = []
+        
+        for i in range(3):
+            row = []
+            for j in range(3):
+                rect = pygame.Rect(tile_origin[0] + j * tile_size,
+                                   tile_origin[1] + i * tile_size,
+                                   tile_size, tile_size)
+                pygame.draw.rect(screen, white, rect, 3)
+
+                if board[i][j] != ttt.EMPTY:
+                    move = moveFont.render(board[i][j], True, white)
+                    moveRect = move.get_rect()
+                    moveRect.center = rect.center
+                    screen.blit(move, moveRect)
+                row.append(rect)
+            tiles.append(row)
+
+
+    pygame.display.flip()
