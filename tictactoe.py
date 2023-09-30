0b3bf179e3524e3bddbb1b27f3d9491af6987ee3
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-09-29 21:01:08 -0700

    Set up the requires functions for minimax algorithm

---
+++ tictactoe.py
@@ -0,0 +1,32 @@
+
+# Creates an empty board
+def initial_state():
+    return
+
+# Returns player who has the next turn on a board
+def player(board):
+    return
+
+# Returns set of all possible actions (i, j) available on the board
+def actions(board):
+    return    
+
+# Returns the board that results from making move (i,j)
+def result(board, action):
+    return
+
+# Returns the winner of the game, if there is one
+def winner(board):
+    return
+
+# Returns True if game is over, False otherwise
+def terminal(board):
+    return
+
+# Returns 1 if X has won the game, -1 if O has won, 0 otherwise
+def utility(board):
+    return
+
+# Returns the optimal action for the current player on the board
+def minimax(board):
+    return
