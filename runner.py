e9b25da0e111040a0bfe9ab281d5b6c7b079e31d
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-09-30 15:45:56 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -6,18 +6,18 @@ import tictactoe as ttt
 
 # Initialize pygame
 pygame.init()
-size = width, height = 600, 600
+size = width, height = 600, 400
 
 # Colors
-black = 0, 0, 0
-white = 255, 255, 255
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 # Create screen
 screen = pygame.display.set_mode(size)
 
-mediumFont = pygame.font.Font("\\OpenSans-Regular.ttf", 28)
-largeFont = pygame.font.Font("\\OpenSans-Regular.ttf", 40)
-moveFont = pygame.font.Font("\\OpenSans-Regular.ttf", 60)
+mediumFont = pygame.font.Font("OpenSans-Regular.ttf", 28)
+largeFont = pygame.font.Font("OpenSans-Regular.ttf", 40)
+moveFont = pygame.font.Font("OpenSans-Regular.ttf", 60)
 
 
 user = None
@@ -40,3 +40,19 @@ while True:
         titleRect = title.get_rect()
         titleRect.center = ((width / 2), 50)
         screen.blit(title, titleRect)
+        
+        # Buttons
+        playXButton = pygame.Rect((width / 8), (height / 2), (width / 4), 50)
+        playOButton = pygame.Rect(5 * (width / 8), (height / 2), (width / 4), 50)
+        
+        playX = mediumFont.render("Play as X", True, black)
+        playXRect = playX.get_rect()
+        playXRect.center = playXButton.center
+        pygame.draw.rect(screen, white, playXButton)
+        screen.blit(playX, playXRect)
+        
+        playO = mediumFont.render("Play as O", True, black)
+        playORect = playO.get_rect()
+        playORect.center = playOButton.center
+        pygame.draw.rect(screen, white, playOButton)
+        screen.blit(playO, playORect)
