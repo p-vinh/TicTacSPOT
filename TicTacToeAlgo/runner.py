a682a1f84aed6463434b1649da93499369985b02
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-10-22 01:23:04 -0700

    Moved to new virtual environment

--- TicTacToeAlgo/runner.py
+++ TicTacToeAlgo/runner.py
@@ -15,7 +15,7 @@ white = (255, 255, 255)
 # Create screen
 screen = pygame.display.set_mode(size)
 
-mediumFont = pygame.font.Font(".\\TicTacToeAlgo\\OpenSans-Regular.ttf", 28)
+mediumFont = pygame.font.Font("TicTacToeAlgo\\OpenSans-Regular.ttf", 28)
 largeFont = pygame.font.Font("TicTacToeAlgo\\OpenSans-Regular.ttf", 40)
 moveFont = pygame.font.Font("TicTacToeAlgo\\OpenSans-Regular.ttf", 60)
 
